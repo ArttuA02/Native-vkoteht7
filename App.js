071dd8cd4be2a95ca951d1e47ab10d1d99fbd1f1
Arttua_02 <t1aaar01@students.oamk.fi>
2024-10-15 14:09:33 +0300

    valmis vkoteht7

--- App.js
+++ App.js
@@ -1,17 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Button, FlatList, Text, Pressable } from 'react-native';
+import React, {useReducer, useState} from 'react';
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, {text: action.text, completed: false}]
+    case 'REMOVE_TODO':
+      return state.filter((todo, index) => index !== action.index)
+    }
+  }
 
 export default function App() {
+  const [todos, dispatch] = useReducer(todoReducer, [])
+  const [text, setText] = useState('');
+
+  function handleAddTodo() {
+    dispatch({ type: 'ADD_TODO', text });
+    setText('');
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <TextInput 
+        value={text} 
+        onChangeText={setText} 
+        placeholder="Task..."
+        />
+        <Button title='Save' onPress={() => handleAddTodo()} />
+
+        <FlatList 
+          style={{marginTop: 10}}
+          data={todos}
+          renderItem={({item, index}) => (
+            <Pressable onPress={() => dispatch({ type: 'REMOVE_TODO', index })}>
+              <View>
+                <Text style={{fontSize: 19}}>{item.text}</Text>
+              </View>
+            </Pressable>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 300,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
